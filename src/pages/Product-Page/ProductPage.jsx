02184d81fb62491d7863891ce850a1da3c0b3d41
rutu14 <vidyapunjabi7@gmail.com>
2022-03-20 22:04:10 +0530

    feat: Product Page components with functionality

---
+++ src/pages/Product-Page/ProductPage.jsx
@@ -0,0 +1,28 @@
+import { useContext, useEffect, useState } from 'react';
+import { FilterSection, ProductCard, Loader, Alert } from '../../components';
+import { ProductContext } from '../../context';
+import { useFilters } from '../../reducers';
+import './product-page.css'
+
+const ProductPage = () => {
+    const [ viewAlert, setViewAlert ] = useState(false);
+    const { product } = useContext(ProductContext); 
+	const { loader, products, error } = product;
+    const{ inventoryData, state, dispatch } = useFilters( );
+    const handleOutOfStock = () => setViewAlert(true);
+    const closeAlert = () => setViewAlert(false);
+    return (
+        <main className="product-list">
+            <FilterSection state={state} dispatch={dispatch} />
+            <section className="product-grid grid-4-cols"> 
+             { viewAlert && <Alert action={'warning'} closefn={closeAlert} title={'Out of Stock'} description={'The product is currently out of stock'}/>}
+                { loader && <Loader/>}
+                {inventoryData && inventoryData.map((item) => (
+                    <ProductCard key={item._id} cardValue={item} handleOutOfStock={handleOutOfStock}/>
+                )) }                                  
+            </section>   
+        </main>
+    );
+}
+
+export { ProductPage }
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+import { useFilters } from "./filterProductsReducer";
+
+export { useFilters }
