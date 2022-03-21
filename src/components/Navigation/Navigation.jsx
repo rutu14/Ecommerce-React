b8d19f62fef76b7b956fa85aebc1223fc77a205f
rutu14 <vidyapunjabi7@gmail.com>
2022-03-21 20:14:38 +0530

    refactor: minor fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,13 +3,18 @@ import logo from '../../asset/image/logo.svg'
 import { MobileNavigation } from './MobileNavigation';
 import { NavLinks } from './NavLinks';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../context';
 
 const Navigation = () => {
+    const [ avatarName,setAvatarName ] = useState(" ")
     const { state } = useContext(UserContext);
     const { tokenPresent, userInfo } = state;
-    const avatarName = userInfo.foundUser.firstName.charAt(0)+userInfo.foundUser.lastName.charAt(0)
+    useEffect(()=>{
+        if( tokenPresent){
+            setAvatarName( userInfo.foundUser.firstName.charAt(0)+userInfo.foundUser.lastName.charAt(0) )
+        }
+    },[ tokenPresent])
 
     return(
         <header className="nav-header header">
@@ -24,7 +29,7 @@ const Navigation = () => {
         </div>
         {tokenPresent
         ? <div className="avatar avatar-text text-md mobile-avatar">{avatarName}</div>  
-        : <Link to={'login'} className="btn btn-primary outline login-sign" type="button">Login</Link> 
+        : <Link to={'login'} className="btn btn-primary outline login-sign mobile-avatar" type="button">Login</Link> 
         }             
         <form className="input-grp input-pos" id="searchform">
             <input type="text" id="searchterm" className="input-grp-right text-input input-width inp-border" />
